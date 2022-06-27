08d9a699792914c8cfb82e89ffe665ffe645920b
rasvindra <rasvindra@hotmail.com>
2022-06-26 20:30:43 -0700

    Attaching promts to button click, and creating random string

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 // Assignment Code
 
-var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 
-function writePassword() {
-  var password = generatePassword(
+/*On button click promt*/
+function parameters(){
 
-
-  );
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var alpha = prompt("Would you like to include letters? Please enter Y for yes or N or no.")
+if (alpha !=="Y" && alpha !=="N") alert("Not a valid choice!")
+  else var numeric = prompt("Would you like to include numbers? Please enter Y for yes or N or no.")
+  if (numeric !=="Y" && numeric !=="N") alert("Not a valid choice!")
+  else var special = prompt("Would you like to include special characters? Please enter Y for yes or N or no.")
+  if (special !=="Y" && special !=="N") alert("Not a valid choice!");
+  //  Else statement here
 }
 
+function writePassword(length, chars) {
+  var how = '';
+  if (chars.indexOf('a') > -1) how += 'abcdefghijklmnopqrstuvwxyz';
+  if (chars.indexOf('A') > -1) how += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  if (chars.indexOf('#') > -1) how += '0123456789';
+  if (chars.indexOf('!') > -1) how += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+  var result = '';
+  for (var i = length; i > 0; --i) result += how[Math.floor(Math.random() * how.length)];
+  return result;
+}
 
+console.log(writePassword(128, "#aA!"));
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-<button onclick="password()">Click me</button>
+
+<button id="generate" onclick="parameters()">
+</button>
+
 
 // var randomNum  = Math.random()
 // console.log(randomNum)
@@ -29,8 +41,3 @@ generateBtn.addEventListener("click", writePassword);
 // var roundDown = Math.floor(numTimesLength)
 // console.log(roundDown)
 
-console.log(names[Math.floor(Math.random()*/*whatever amount of randomn number*/)])
-
-/* Example of random text generator */
-<input type='text' id='p'/><br/>
-<input type='button' value ='generate' onclick='document.getElementById("p").value = Password.generate(16)'></input>
