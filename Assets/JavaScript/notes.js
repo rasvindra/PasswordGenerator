74480e165f217c5ffe7f7fe0793e0710d1494235
rasvindra <rasvindra@hotmail.com>
2022-06-27 22:43:12 -0700

    running wrong response in prompts to make sure nothing breaks

--- Assets/JavaScript/notes.js
+++ Assets/JavaScript/notes.js
@@ -5,67 +5,59 @@ var numberChar =["0","1","2","3","4","5","6","7","8","9"]
 var specialChar =["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","'","<",">","?",".","/","|",'"',","];
 
 function parameters() {
-    var alphaLower = prompt("Would you like to include lower case   letters? Please enter Y for yes or N or no.")
-    if (alphaLower !== "Y" || alphaLower !== "N")
+    var alphaLower = prompt("Would you like to include lower case  letters? Please enter Y for yes or N or no.")
+    if (alphaLower !== "Y" && alphaLower !== "N")
         alert("Not a valid choice!");
     var alphaUpper = prompt(
       "Would you like to include upper case letters? Please enter Y for yes or N or no.")
-    if (alphaUpper !== "Y" || alphaUpper !== "N") 
+    if (alphaUpper !== "Y" && alphaUpper !== "N") 
       alert("Not a valid choice!")
     var numberValues= prompt(
         "Would you like to include numbers? Please enter Y for yes or N or no.");
-    if (numberValues !== "Y" || numberValues !== "N") 
+    if (numberValues !== "Y" && numberValues !== "N") 
       alert("Not a valid choice!");
     var specialValues = prompt(
         "Would you like to include special characters? Please enter Y for yes or N or no.");
-    if (specialValues !== "Y" || specialValues !== "N") alert("Not a valid choice!");
-      var numofChar = prompt(
+    if (specialValues !== "Y" && specialValues !== "N") alert("Not a valid choice!");
+      var numofChar = parseInt( prompt(
         "How many charaters long would you like your password to be? Please enter a value no less than 8 and no more than 128"
-      );
+      ));
     if (numofChar < 8 || numofChar > 128)
-      alert("Not a valid choice! Please retry.");
+      alert("Not a valid choice!");
 
-    if (alphaLower === "N" && alphaUpper === "N" && specialValues === "N" && numberValues === "N"){
-      alert("Character's not selected! No Password craeated!")
+    if (alphaLower !== "Y" && alphaUpper !== "Y" && specialValues !== "Y" && numberValues !== "Y"){
+      alert("Character Type not selected! No Password created!")
     } 
     else {
       var allUserChoices = [];
-      if(specialChar ==="Y"){
+      if(specialValues ==="Y"){
           allUserChoices = allUserChoices.concat(specialChar)
       }
-      if(numberChar ==="Y"){
+      if(numberValues ==="Y"){
           allUserChoices = allUserChoices.concat(numberChar)
       }
-      if(alphalwrChar ==="Y"){
+      if(alphaLower ==="Y"){
           allUserChoices = allUserChoices.concat(alphalwrChar)
       }
-      if(alphauprChar ==="Y"){
+      if(alphaUpper ==="Y"){
         allUserChoices = allUserChoices.concat(alphauprChar)
       }
+    
       writePassword(numofChar, allUserChoices)  
     }
   }
   
-
-   // var results =
-    //loop through the allUserChpices array numofChar times to get a password
-        // and push result to results
-   
-    // return results.join('');
     function writePassword(numofChar,allUserChoices){
         var password = ""
-        for(var i = 0; i > 0; i++)  
-          password=passwrod+allUserChoices[Math.floor(Math.random() * numofChar)]
-9
+        for(var i = 0; i < numofChar; i++){
+          password=password+allUserChoices[Math.floor(Math.random() * numofChar)]
+          console.log(Math.floor(Math.random() * numofChar))
+        }
+        console.log(password)
        var passwordText = document.querySelector("#password");
        passwordText.value = password;
     }
 
     // Add event listener to generate button
     document.querySelector("#generate").addEventListener("click", parameters)
-    
-
-function writePassword() {
-    var password = parameters();
-
-}
+   
